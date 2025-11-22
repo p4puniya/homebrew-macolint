8150f6f1f5e54c35e7afeee05dd8126949a38a0f
gaurav-puniya <gaurav.puniya@greyorange.com>
2025-11-22 18:02:23 +0530

    Added formula

--- Formula/macolint.rb
+++ Formula/macolint.rb
@@ -6,7 +6,7 @@ class Macolint < Formula
   desc "A terminal-first snippet manager with encrypted local storage"
   homepage "https://github.com/p4puniya/macolint"
   url "https://github.com/p4puniya/macolint/archive/v0.1.0.tar.gz"
-  sha256 "" # Update with actual SHA256 after first release
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5" # Update with actual SHA256 after first release
   license "MIT"
   head "https://github.com/p4puniya/macolint.git", branch: "main"
 
